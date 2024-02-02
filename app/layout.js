de72a6a844e96e966c80977d08ea06294401591b
Tobias <tobyajras@gmail.com>
2024-02-02 01:13:59 -0300

    starting footer

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ReactQuery from "@/utils/ReactQuery";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       >
         <Navbar />
         <ReactQuery>{children}</ReactQuery>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className='flex justify-center'>
+      <div className='max-w-6xl'>
+        <section>
+          <article>
+            <div>
+              <Image />
+            </div>
+            <div></div>
+          </article>
+          <article></article>
+          <article></article>
+          <article></article>
+        </section>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
