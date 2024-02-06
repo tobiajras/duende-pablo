2b3c5633815b2b8ff58331f9ea8cea16aefb03ae
Tobias <tobyajras@gmail.com>
2024-02-06 00:17:51 -0300

    working in sobre-mi

--- app/sobre-mi/page.jsx
+++ app/sobre-mi/page.jsx
@@ -5,30 +5,30 @@ import historia from '@/data/historia.json';
 const SobreMi = () => {
   return (
     <section className='flex flex-col items-center'>
-      <div className='flex flex-col items-center max-w-6xl gap-20 my-40 mx-10'>
+      <div className='flex flex-col items-center max-w-6xl gap-10 md:gap-20 my-20 md:my-40 mx-4 md:mx-10'>
         {historia.map((historiaItem, idx) =>
           idx % 2 === 0 ? (
             <article
               key={historiaItem.id}
-              className='flex flex-col md:flex-row items-center gap-10'
+              className='flex flex-col md:flex-row items-center gap-5 md:gap-10'
             >
               <div className=''>
                 <Image
-                  className='w-full h-full object-contain grayscale hover:grayscale-0 transition duration-500 rounded-xl'
+                  className='w-full h-full object-contain grayscale hover:grayscale-0 transition duration-500 rounded-md md:rounded-xl'
                   src={historiaItem.image}
                   alt={historiaItem.alt}
                   width={400}
                   height={400}
                 />
               </div>
-              <div className='flex gap-5'>
+              <div className='flex gap-3 md:gap-5'>
                 <div className='bg-primary w-1.5 rounded-full'></div>
-                <div className='py-3 max-w-lg'>
-                  <h4 className='text-xl text-text-primary font-semibold mb-3'>
+                <div className='py-1 md:py-3 max-w-lg'>
+                  <h4 className='md:text-xl text-text-primary font-semibold mb-2 md:mb-3'>
                     {historiaItem.title}
                   </h4>
                   {historiaItem.description.map((historiaP, idx) => (
-                    <p key={idx} className='mb-2'>
+                    <p key={idx} className='mb-1 md:mb-2 text-sm sm:text-base'>
                       {historiaP}
                     </p>
                   ))}
@@ -36,30 +36,25 @@ const SobreMi = () => {
               </div>
             </article>
           ) : (
-            <article className='flex items-center gap-10'>
-              <div className='flex gap-4'>
+            <article className='flex flex-col-reverse md:flex-row items-center gap-5 md:gap-10'>
+              <div className='flex gap-3 md:gap-5'>
                 <div className='bg-primary w-1.5 rounded-full'></div>
-                <div className='py-3 max-w-lg'>
-                  <h4 className='text-xl text-text-primary font-semibold mb-3'>
-                    Carrera y Aficiones
+                <div className='py-1 md:py-3 max-w-lg'>
+                  <h4 className='md:text-xl text-text-primary font-semibold mb-2 md:mb-3'>
+                    {historiaItem.title}
                   </h4>
-                  <p className='mb-2'>
-                    A pesar de estudiar y trabajar en medicina, Pablo mantuvo
-                    sus pasatiempos, como Magic, el arte y los juegos
-                    competitivos.
-                  </p>
-                  <p>
-                    Su primer salario como médico lo invirtió en una
-                    computadora, con la que mejoró en juegos como &ldquo;League
-                    of Legends&ldquo; y &ldquo;Magic&ldquo;.
-                  </p>
+                  {historiaItem.description.map((historiaP, idx) => (
+                    <p key={idx} className='mb-1 md:mb-2 text-sm sm:text-base'>
+                      {historiaP}
+                    </p>
+                  ))}
                 </div>
               </div>
               <div className=''>
                 <Image
                   className='w-full h-full object-contain grayscale hover:grayscale-0 transition duration-500 rounded-xl'
-                  src='/assets/sobre-mi/img2-sobremi-duende.webp'
-                  alt='img1-sobremi'
+                  src={historiaItem.image}
+                  alt={historiaItem.alt}
                   width={400}
                   height={400}
                 />
