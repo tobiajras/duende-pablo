bb35f4e6464b672e64627eb8a458f2350ba48506
Tobias <tobyajras@gmail.com>
2024-02-13 16:32:20 -0300

    metadataBase

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,14 @@ import Footer from '@/components/Footer';
 // const inter = Inter({ subsets: ["latin"] });
 const assistant = Assistant({ subsets: ['latin'] });
 
+export const metadata = {
+  metadataBase: new URL('http://localhost:3000'),
+  title: 'Duende Pablo - Streamer Argentino',
+  description: 'Creador de contenido de Twitch y Youtube - Argentina',
+  openGraph: {
+    images: '/inicio-og-duende.png',
+  },
+};
 // title: 'Duende Pablo - Streamer Argentino',
 // description: 'Creador de contenido de Twitch y Youtube - Argentina',
 
