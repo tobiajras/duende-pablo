57a627205e1705b5b856e4af8b448d43deb4a2af
Tobias <tobyajras@gmail.com>
2024-02-14 19:47:28 -0300

    metadata object metadata removed

--- app/layout.js
+++ app/layout.js
@@ -14,14 +14,12 @@ export const metadata = {
     title: 'Duende Pablo - Streamer Argentino',
     description: 'Creador de contenido de Twitch y Youtube - Argentina',
     url: 'https://duende-pablo.vercel.app/',
-    image: '/assets/opengraph-image.png',
     type: 'website',
   },
   twitter: {
     card: 'summary_large_image',
     title: 'Duende Pablo - Streamer Argentino',
     description: 'Creador de contenido de Twitch y Youtube - Argentina',
-    image: ['/assets/twitter-image.png'],
   },
 };
 
