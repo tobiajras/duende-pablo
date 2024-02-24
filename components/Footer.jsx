c8a0450209d0c9a8581fd25a2ce7e573caea7aa1
Tobias <tobyajras@gmail.com>
2024-02-24 16:25:06 -0300

    next_public added to vercel settings

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -157,7 +157,7 @@ const Footer = () => {
           </article>
         </section>
       </div>
-      <div className='bg-black text-base py-3 flex gap-2 justify-center a'>
+      <div className='bg-black text-base py-3 flex gap-2 justify-center'>
         <span>Desarrollado por:</span>
         <Link
           className='hover:text-text-primary transition-colors'
